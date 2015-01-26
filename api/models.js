d5e621fac616c799e7a3e75b8fee0ca4097af26b
Paul Wijhenke <paul@paulsplace.nl>
2015-01-26 17:21:18 +0100

    Some more thoughts about Api server and models

--- api/models.js
+++ api/models.js
@@ -2,6 +2,14 @@
 "use strict";
 
 
+var categories = [
+  'public',
+  'personal',
+  'informative',
+  'funny',
+];
+
+
 /**
  * User model
  */
@@ -24,6 +32,60 @@ function createUserModel(obj)  {
   obj.User.ensureIndex('username');
   obj.User.ensureIndex('email');
   obj.User.ensureIndex('authToken');
+}
+
+
+/**
+ * Contacts
+ */
+function createContactsModel(obj) {
+  obj.Contact = obj.db.createModel('friends', {
+    id: String,
+
+    userId: String,
+
+    friendId: String,
+    friendName: String,
+    following: Boolean,
+
+    accepted: Date,
+  });
+}
+
+
+/**
+ * TimelineItems
+ */
+function createTimelineItem(obj) {
+  obj.TimelineItem = obj.db.createModel('timelineitem', {
+    id: String,
+
+    ownerId: String,
+    recipients: Array,
+
+    category: String,
+    title: String,
+    body: String,
+    body_url: String,
+
+    created: String,
+  });
+}
+
+
+/**
+ * TimelineItems
+ */
+function createNotifications(obj) {
+  obj.Notification = obj.db.createModel('notifications', {
+    userId: String,
+
+    type: String,
+    title: String,
+
+    created: String,
+    read: Date
+  });
 }
 
 
