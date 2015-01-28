4371be81493716f7015a35f88b351b38eb3896c1
Harrie Pieters <harrie@harriepieters.nl>
2015-01-28 21:15:00 +0100

    Fixed guid error

--- web/static/js/login.js
+++ web/static/js/login.js
@@ -1,4 +1,4 @@
-riot.tag('login', '<div class="row"> <h1>{ page.title }</h1> <div id="error" class="{ errorState ? \'show\' : \'hide\' }"><p>Oops, something went wrong</p></div> <form id="login" onsubmit="{ submit }"> <input name="username" id="username" type="text" placeholder="username"> <input name="password" id="password" type="password" placeholder="password"> <button name="submit">Submit</button> </form> </div>', function(opts) {
+riot.tag('login', '<div class="row"> <h1>{ page.title }</h1> <div id="error"><p>Oops, something went wrong</p></div> <form id="login" onsubmit="{ submit }"> <input name="username" id="username" type="text" placeholder="username"> <input name="password" id="password" type="password" placeholder="password"> <button name="submit">Submit</button> </form> </div>', function(opts) {
         var form = this.login,
             button = this.submit
 
@@ -7,12 +7,10 @@ riot.tag('login', '<div class="row"> <h1>{ page.title }</h1> <div id="error" cla
             var username = this.username.value,
                 password = this.password.value;
 
-            var uuid = guid();
+            console.log('Emit - Authenticate');
+            io.emit('authenticate', { username: username, password: password});
 
-            console.log('Emit - Authenticate', uuid);
-            socket.emit('authenticate', { username: username, password: password, requestId: uuid});
-
-            socket.on('authenticateResponse', function (data) {
+            io.on('authenticateResponse', function (data) {
                 console.log('On - authenticateResponse', data);
 
                 if (data.err) {
