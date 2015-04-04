4ce2cff6d14cebe8c22ee05a7fcc3871c6a124f2
Paul Wijhenke <paul@paulsplace.nl>
2015-04-04 12:47:39 +0200

    XmppServer: Authenticate against DB

--- xmppserver/xmppserver.js
+++ xmppserver/xmppserver.js
@@ -1,5 +1,6 @@
 /* jshint node: true */
 "use strict";
+var util = require('util');
 
 var xmpp = require('node-xmpp-server');
 var ltx = require('node-xmpp-core').ltx;
@@ -19,6 +20,7 @@ function XmppServer(iface, qname, options) {
 
   // The server
   this.server = null;
+  this.apiClient = null;
 
   // Setup
   this.config = options.config;
@@ -29,12 +31,55 @@ function XmppServer(iface, qname, options) {
 XmppServer.prototype.onStart = function (done) {
   var self = this;
 
+  // Start XMPP server
   this.server = new xmpp.C2SServer({
       port: 5222,
       domain: 'localhost'
   });
-  this.server.on("connect", this.onConnect);
-  this.server.on("register", this.onRegister);
+
+  this.server.on("connect", function(client) {
+    // Get clients
+    self.apiClient = self.iface.clients.scatter_api;
+
+    // Client methods
+    client.on("authenticate", function(opts, cb) {
+      var userEmail = util.format('%s@%s', opts.username, opts.jid.domain);
+      var authData = {
+        username: userEmail,
+        password: opts.password
+      };
+      self.apiClient.userGetAuthToken(authData, function (err) {
+        if (err) {
+          console.log('!* authenticate failed', {user: userEmail});
+          return cb(false);
+        }
+        return cb(null, opts);
+      });
+    });
+
+    client.on("online", function() {
+      console.log('!****** new client');
+      // var msg = new ltx.Element('message', { to: 'localhost' }).c('body').t('HelloWorld');
+      // client.send(msg);
+    });
+
+    // Stanza handling
+    client.on("stanza", function(stanza) {
+      console.log('stanza: ', stanza);
+    });
+
+    // On Disconnect event. When a client disconnects
+    client.on("disconnect", function(client) {
+      console.log('client disconnected');
+    });
+
+  });
+
+  this.server.on("register", function onRegister(opts, done) {
+    console.log('!****** onRegister', opts);
+    return done(true);
+  });
+
   return done();
 };
 
@@ -43,39 +88,6 @@ XmppServer.prototype.onStop = function (done) {
   return done();
 };
 
-
-XmppServer.prototype.onRegister = function onRegister(opts, done) {
-  var self = this;
-  console.log('!****** onRegister', opts);
-  done(true);
-};
-
-
-XmppServer.prototype.onConnect = function(client) {
-  var self = this;
-
-  client.on("authenticate", function(opts, cb) {
-    console.log('!****** authenticate', opts);
-    return cb(null, opts);
-  });
-
-  client.on("online", function() {
-    console.log('!****** new client');
-    // var msg = new ltx.Element('message', { to: 'localhost' }).c('body').t('HelloWorld');
-    // client.send(msg);
-  });
-
-  // Stanza handling
-  client.on("stanza", function(stanza) {
-    console.log('stanza: ', stanza);
-  });
-
-  // On Disconnect event. When a client disconnects
-  client.on("disconnect", function(client) {
-    console.log('client disconnected');
-  });
-
-};
 
 
 // exports
