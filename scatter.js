a103ecb03ebf402d7381225b6b1d5b8332785e4a
Paul Wijhenke <paul@paulsplace.nl>
2015-04-26 19:26:37 +0200

    Style fixes

--- scatter.js
+++ scatter.js
@@ -2,12 +2,10 @@
 "use strict";
 var path = require('path');
 
-// The micro-service manager
 var m1cro = require('m1cro');
 
 
 // Configuration and constants
-//
 var cfg = require(path.join(__dirname, 'lib', 'config'));
 
 // Services that make up our Application
@@ -19,12 +17,10 @@ var XmppServer = require(path.join(__dirname, 'xmpp', 'xmppserver'));
 
 // Setup Interface using loopback transport (default)
 // Setup our services .. and start!
-//
 var iface = m1cro.interface({appName: 'scatter'});
 
 
 // Attach services to the Service Manager
-//
 iface.service(ApiService, cfg.constants.api.queue.name, {
     config: cfg.config.api
 });
