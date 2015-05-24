2fca4ee2927d8718ecb52e938c3640ec869f565b
HPieters <harrie@harriepieters.nl>
2015-05-24 13:20:45 +0200

    Added register code

--- web/src/tag/register.tag
+++ web/src/tag/register.tag
@@ -6,7 +6,7 @@
             </div>
             <div id="error" class={ errorState ? 'error-container show' : 'error-container' }><p> { errorMessage }</p></div>
             <form id="register" onsubmit={ submit }>
-                <input name="email" class="input" id="email" type="text" placeholder="Email">
+                <!-- <input name="email" class="input" id="email" type="text" placeholder="Email"> -->
                 <input name="username" class="input" id="username" type="text" placeholder="Username (full domain)">
                 <input name="password" class="input" id="password" type="password" placeholder="Password">
                 <button name="submit" class="submit">Register</button>
@@ -19,31 +19,54 @@
     </div>
 
     <script>
+        /* jshint ignore:start */
         document.title = 'Scatter - Register';
 
         var self = this;
 
         submit(e) {
-            var data = {
-                email: self.email.value.trim(),
-                username: self.username.value.trim(),
-                password: self.password.value.trim()
-            }
+            e.preventDefault();
+
+            // var email: self.email.value.trim(),
+            var username: self.username.value.trim();
+            var password: self.password.value.trim();
+
             console.log('[Register.js] Registering ', data);
 
-            io.emit('userSignUp', data);
+            // io.emit('userSignUp', data);
 
-            io.on('userSignUpResponse', function (response) {
-                console.log('[Register.js] Server response', response);
-                if (response.err) {
-                    self.errorState = true;
-                    self.errorMessage = response.err.message || 'Unknown';
-                    riot.update();
-                    return false;
+            // io.on('userSignUpResponse', function (response) {
+            //     console.log('[Register.js] Server response', response);
+            //     if (response.err) {
+            //         self.errorState = true;
+            //         self.errorMessage = response.err.message || 'Unknown';
+            //         riot.update();
+            //         return false;
+            //     }
+            //     console.log('Great Succes, you are now a member!', response.result);
+            //     riot.route('login');
+            // })
+
+            var client = new XMPP.Client({
+                websocket: { url: 'ws://dev.local:5280' },
+                jid: username,
+                password: password,
+                register: true
+            });
+
+            client.addListener(
+                'online',
+                function() {
+                    console.log('online')
                 }
-                console.log('Greate Succes, you are now a member!', response.result);
-                riot.route('login');
-            })
+            )
+
+            client.addListener(
+                'error',
+                function(e) {
+                    console.error(e)
+                }
+            )
 
         }
     </script>
--- web/static/js/register.js
+++ web/static/js/register.js
@@ -1,29 +1,49 @@
-riot.tag('register', '<div id="register-container"> <div class="register-content"> <div class="logo-container"> <div class="logo"></div> </div> <div id="error" class="{ errorState ? \'error-container show\' : \'error-container\' }"><p> { errorMessage }</p></div> <form id="register" onsubmit="{ submit }"> <input name="email" class="input" id="email" type="text" placeholder="Email"> <input name="username" class="input" id="username" type="text" placeholder="Username (full domain)"> <input name="password" class="input" id="password" type="password" placeholder="Password"> <button name="submit" class="submit">Register</button> </form> <div class="unauthenticated-footer-links"> <div class="unauthenticated-footer-links-left"><a href="#">Login</a></div> <div class="unauthenticated-footer-links-right"><a href="#forgot-password">Forgot your password?</a></div> </div> </div> </div>', function(opts) {
+riot.tag('register', '<div id="register-container"> <div class="register-content"> <div class="logo-container"> <div class="logo"></div> </div> <div id="error" class="{ errorState ? \'error-container show\' : \'error-container\' }"><p> { errorMessage }</p></div> <form id="register" onsubmit="{ submit }">  <input name="username" class="input" id="username" type="text" placeholder="Username (full domain)"> <input name="password" class="input" id="password" type="password" placeholder="Password"> <button name="submit" class="submit">Register</button> </form> <div class="unauthenticated-footer-links"> <div class="unauthenticated-footer-links-left"><a href="#">Login</a></div> <div class="unauthenticated-footer-links-right"><a href="#forgot-password">Forgot your password?</a></div> </div> </div> </div>', function(opts) {
+        
         document.title = 'Scatter - Register';
 
         var self = this;
 
         this.submit = function(e) {
-            var data = {
-                email: self.email.value.trim(),
-                username: self.username.value.trim(),
-                password: self.password.value.trim()
-            }
+            e.preventDefault();
+
+            var username: self.username.value.trim();
+            var password: self.password.value.trim();
+
             console.log('[Register.js] Registering ', data);
 
-            io.emit('userSignUp', data);
 
-            io.on('userSignUpResponse', function (response) {
-                console.log('[Register.js] Server response', response);
-                if (response.err) {
-                    self.errorState = true;
-                    self.errorMessage = response.err.message || 'Unknown';
-                    riot.update();
-                    return false;
+
+
+
+
+
+
+
+
+
+
+
+            var client = new XMPP.Client({
+                websocket: { url: 'ws://dev.local:5280' },
+                jid: username,
+                password: password,
+                register: true
+            });
+
+            client.addListener(
+                'online',
+                function() {
+                    console.log('online')
                 }
-                console.log('Greate Succes, you are now a member!', response.result);
-                riot.route('login');
-            })
+            )
+
+            client.addListener(
+                'error',
+                function(e) {
+                    console.error(e)
+                }
+            )
 
         }.bind(this);
     
